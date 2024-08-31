906d2973408ba52a9873443daa5e503097f84994
David Pontes <pondavid103@gmail.com>
2024-08-31 05:13:32 -0300

    Initialize project using Create React App

---
+++ src/Rotina.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const Rotina = () => {
+  return (
+    <div>
+      <h1><b>Rotina</b></h1>
+      <br />
+      <p><a href="contato.html">Contato</a></p>
+      <p>Foto</p>
+      <br />
+      <img src="https://christianosantos.com/files/cs1/avatar-homem.jpg" alt="Foto" />
+      <br />
+      <video src="https://www.w3schools.com/tags/movie.mp4" controls></video>
+      <br />
+      <audio loop src="http://www.w3schools.com/tags/horse.mp3" controls>Audio</audio>
+      <p>
+        Acordar (07:00): Levante-se da cama e estique-se para despertar o corpo.
+        <br />
+        Higiene Pessoal (07:05 - 07:15): Lave o rosto, escove os dentes e tome um banho rápido.
+        <br />
+        Café da Manhã (07:15 - 07:30): Prepare e tome um café da manhã saudável.
+        <br />
+        Planejamento do Dia (07:30 - 07:40): Revise suas tarefas e objetivos do dia.
+        <br />
+        Exercício (07:40 - 08:00): Faça uma caminhada leve ou uma série de exercícios simples.
+        <br />
+        Trabalho/Estudo (08:00 - 12:00): Concentre-se nas principais tarefas ou no estudo.
+        <br />
+        Pausa para o Almoço (12:00 - 12:30): Almoce e descanse um pouco.
+      </p>
+      <hr />
+      <br />
+      <p><i>“Só sei que nada sei.”</i></p>
+      <p>~Sócrates</p>
+    </div>
+  );
+}
+
+export default Rotina;
